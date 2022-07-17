bb805117434124838939315997a8ee89b33983d8
Brian <brainmadro@gmail.com>
2022-07-16 21:38:31 -0500

    Firt init

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/e', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
