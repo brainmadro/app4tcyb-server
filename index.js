740854e1c2ed6b70f1b556f9cbe117f4a0502ca2
Roberto Polo <roberto.polo@outlook.com>
2023-03-12 21:27:05 -0500

    Agregar endpoint interviews

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const app = require('./routes')
+const express = require("express")
+const path = require('path')
 const PORT = process.env.PORT || 5000
+const v1InterviewRouter = require("./src/v1/routes/interviewRoutes");
+
+
+const app = express()
+app.use("/api/v1/interviews", v1InterviewRouter);
 
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${ PORT }`))
